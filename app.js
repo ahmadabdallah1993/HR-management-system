39008f8f42f00a48d3673dc5c3eb071467d6ab51
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-02 23:02:47 +0300

    finish

--- app.js
+++ app.js
@@ -30,22 +30,54 @@ var employee = {
         level: "Mid-Senior",
         imageURL: 0,
         salary: 0
-    }
-};
-
-
-console.log("employee id test before adding id function =  " + employee[0].employeeID);
-
-
-
-function setID() {
-    var id = 1000;
+    },
+    setID: function(){
+        var id = 1000;
     for(var i = 0; i < 4; i ++) {
-        employee[i].employeeID = id;
-        console.log("employee id after adding id method =  " + employee[i].employeeID);
+        this[i]["employeeID"] = id;
+        console.log("employee id after adding id method =  " + this[i]["employeeID"]);
         id ++;
 
     }
-}
+    },
+    calculateSalary: function() {
 
-setID();
+        for(var i = 0; i < 4; i ++) {
+            if(this[i]["level"] == "Junior"){
+                this[i]["salary"] = Math.floor(Math.random() * (1000 - 500)) + 500;
+            } else if(this[i]["level"] == "Mid-Senior") {
+                this[i]["salary"] = Math.floor(Math.random() * (1500 - 1000)) + 1000;
+            } else if(this[i]["level"] == "Senior") {
+                this[i]["salary"] = Math.floor(Math.random() * (2000 - 1500)) + 1500;
+    
+        }
+        console.log("employee salary after adding salary method =  " + this[i]["salary"]);
+    }
+    },
+    tax: function() {
+        for(var i = 0; i < 4; i ++) {
+            var j = this[i]["salary"];
+            
+            this[i]["salary"] = j - (j * (0.075));
+            console.log("employee salary after adding tax method =  " + this[i]["salary"]);
+            
+        }
+        
+    }
+    
+    
+    
+};
+
+
+console.log("employee id test before adding id method =  " + employee[0].employeeID);
+
+console.log(employee["setID"]());
+
+
+console.log("employee salary test before adding salary method =  " + employee[0].salary);
+
+console.log(employee["calculateSalary"]());
+
+
+console.log(employee["tax"]());
